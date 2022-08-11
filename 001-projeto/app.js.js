9b3d062ab3cdd6243aa292292104a4cd86630810
manutrevenzoli <110498495+manutrevenzoli@users.noreply.github.com>
2022-08-10 21:14:51 -0300

    Add files via upload

---
+++ 001-projeto/app.js.js
@@ -0,0 +1,17 @@
+
+
+
+
+var msg= "teste"
+console.log (msg)
+console.log (typeof msg)
+// console.log(`Server running at http://${hostname}:${port}/`)
+
+var num = 20
+console.log(num)
+console.log (typeof num)
+// console.log(`Server running at http://${hostname}:${port
+
+
+
+

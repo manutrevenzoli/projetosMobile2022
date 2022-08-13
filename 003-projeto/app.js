ac0240244069b37ec2cedf24ffc9946b48b66a52
manutrevenzoli <110498495+manutrevenzoli@users.noreply.github.com>
2022-08-12 21:00:48 -0300

    Add files via upload

--- 003-projeto/app.js
+++ 003-projeto/app.js
@@ -8,3 +8,9 @@ console.log (novosalario);
 novosalarioinflacao = novosalario - (inflacao * novosalario);
 console.log (novosalarioinflacao)
 
+var strnovosalario = `O novo salário é:\n R$\t\t ${novosalario}`;
+console.log (strnovosalario)
+
+var strnovosalarioinflacao = `O novo salário aplicada inflação é: \n R$ ${novosalarioinflacao}`;
+
+console.log (strnovosalarioinflacao)

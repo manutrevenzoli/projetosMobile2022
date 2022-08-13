852dcff4e1d4a08a30e9bc9ffb7064458a650582
manutrevenzoli <110498495+manutrevenzoli@users.noreply.github.com>
2022-08-12 21:00:17 -0300

    Add files via upload

---
+++ 003-projeto/app.js
@@ -0,0 +1,10 @@
+var salario = 2000.00;
+var aumento = 0.15;    
+var inflacao = 0.07
+
+novosalario = salario + (aumento * salario)
+console.log (novosalario);
+
+novosalarioinflacao = novosalario - (inflacao * novosalario);
+console.log (novosalarioinflacao)
+

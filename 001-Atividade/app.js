82a509193e9f7fa18dcfa6a15f1ea405e02346f7
manutrevenzoli <110498495+manutrevenzoli@users.noreply.github.com>
2022-09-01 21:24:35 -0300

    Add files via upload

---
+++ 001-Atividade/app.js
@@ -0,0 +1,26 @@
+
+var num1 = 5
+var num2 = 2
+var num3 = 4
+var num4 = 6
+var num5 = 8
+var soma = num1+num2+num3+num4+num5
+var media = soma/5
+
+console.log (num1),
+console.log (num2),
+console.log(num3),
+console.log (num4),
+console.log (num5),
+console.log (soma)
+console.log (media)
+
+if(media>5){
+
+      console.log(`Passou com ${media}`)
+
+}
+
+else{
+    console.log (`Não passou`)
+} 

1a1b27cdf18181e2246550dd097e8c3db386eb48
manutrevenzoli <110498495+manutrevenzoli@users.noreply.github.com>
2022-09-22 20:26:48 -0300

    Add files via upload

---
+++ projeto015+2/App.js
@@ -0,0 +1,163 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useEffect, useState, Component } from "react";
+import { StyleSheet, Text, View, Button, Modal, Pressable, Icon } from 'react-native';
+import { Provider, Appbar, Avatar, TextInput, HelperText } from 'react-native-paper';
+
+export default function App() {
+const [firstName, setFirstNameText] = React.useState('');
+const [lastName, setLastNameText] = React.useState('');
+const [email, setEmailText] = React.useState('');
+const [password, setPasswordText] = React.useState('');
+const [msgErr, setmsgErrText] = useState('');
+const [secureTextEntry, setSecureTextEntry] = useState(true);
+const [btnEnable, setbtnEnable] = useState(true);
+const [ctrlForm, setctrlForm] = useState(0);
+
+const validateA11 = () => {
+  //console.log("Validando form");
+  //console.log(validateName());
+  //console.log(validatelastName());
+  //console.log(validatePassword());
+  //console.log(validateMail());
+  if( validateName() == false && validatelastName() == false && validatePassword() == false && validateMail() == false )
+  {
+    setmsgErrText("");
+  }
+  else
+  {
+    setmsgErrText("Há algum campo preenchido incorretamente. Verifique!");
+  }
+
+
+}
+
+const validateStrSize = (text) => {
+  if (text.length <= 3) {
+    console.log("Campo preenchido incorretamente.");
+    setmsgErrText("Campo preenchido incorretamente.");
+    return false;
+  }
+
+}
+
+const validateName = () => {
+  //console.log(firstName.length);
+  //firstName = firstName.trim();
+  var cleanStr=firstName.trim();
+  //console.log(cleanStr);
+  if (cleanStr.length >= 3) {
+    return false;
+  }
+};
+
+const validatelastName = () => {
+  //console.log(firstName.length);
+  //firstName = firstName.trim();
+  var cleanStr=lastName.trim();
+  //console.log(cleanStr);
+  if (cleanStr.length >= 3) {
+    return false;
+  }
+};  
+/*
+https://dev.to/petroskoulianos/3-password-regex-for-your-next-project-53fn
+// more secure regex password must be :
+// more than 8 chars  
+ // at least one number
+ // at least one special character
+*/
+
+const validatePassword = () => {
+  const reg = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/;
+  //console.log(reg.test(password));
+  //console.log(!reg.test(email));
+  return !reg.test(password);
+
+};
+
+const validateMail = () => {
+  const reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/;
+  //console.log(reg.test(email));
+  //console.log(!reg.test(email));
+  return !reg.test(email);
+};
+
+
+  return (
+    <View style={styles.container}>
+       {/* inicio campo nome */}
+      <Text style={styles.labelText}>Nome:</Text>
+      <TextInput style={styles.inputText}
+      label="Nome"
+      placeholder="Informe o seu Nome"
+      onChangeText={firstName =>
+      setFirstNameText(firstName)} />
+      <HelperText type="error" visible={validateName()}>
+        O campo Nome está inválido!
+      </HelperText>
+      {/* fim campo nome */}
+      {/* inicio campo sobrenome */}
+      <Text style={styles.labelText}>Sobrenome</Text>
+      <TextInput style={styles.inputText}
+      label="Sobrenome"
+      placeholder="Informe o sobrenome" onChangeText={lastName => setLastNameText(lastName)} defaultValue={lastName} />
+      <HelperText type="error" visible={validatelastName()}>
+        O campo Sobrenome está inválido!
+      </HelperText>
+         {/* fim campo sobrenome */}
+      {/* inicio campo email */}
+      <Text style={styles.labelText}>Email:</Text>
+      <TextInput style={styles.inputText}
+      label="E-mail" 
+      placeholder="Informe seu e-mail" onChangeText={email => setEmailText(email)} defaultValue={email}  />
+      <HelperText type="error" visible={validateMail()}>
+        O campo e-mail está inválido!
+      </HelperText>
+
+      {/* fim campo email */}
+      {/* inicio campo senha */}
+      <Text style={styles.labelText}>Senha:</Text>
+      <TextInput style={styles.inputText}
+      label="Senha"
+      secureTextEntry= {secureTextEntry}
+      placeholder="Informe sua senha" onChangeText={passoword => setPasswordText(passoword)}  right={ <TextInput.Icon name="eye" onPress={() => {setSecureTextEntry(!secureTextEntry); return false;}} />} />
+      <HelperText type="error" visible={validatePassword()}>
+        O campo senha está inválido!
+      </HelperText>
+      {/* fim campo senha */}
+      {/* inicio campo botao */}
+      <Button title="Cadastrar-se" onPress={() =>
+      validateA11()} style={styles.buttonstyle} color="#000000" />
+      
+      <Text style={styles.errorColor} >{msgErr}</Text>
+    </View>
+  
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#696969',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  errorColor: {
+    backgroundColor: 'red',
+  },
+
+  inputText:{
+    height: 50,
+    backgroundColor: '#F5F5F5',
+    marginBottom: 15,
+  },
+
+  labelText:{
+    marginTop: 10,
+    marginBottom: 5,
+  },
+  buttonstyle:{
+    margin: 5,
+  },
+});
